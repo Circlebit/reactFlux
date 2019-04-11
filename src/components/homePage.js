62d9e925451ce4697fab94fb78c24c5deb076133
Paul Seibel <paul.seibel@mailbox.org>
2019-04-11 14:19:09 +0200

    my first component :)

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+            <h1>Pluralsight Administration</h1>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
